0d616db2294244c7c2f51cbeca4a9ac7a3aca533
Tobias Ipsen <71149806+TobiasIpsen@users.noreply.github.com>
2025-03-11 11:13:50 +0100

    wip: healthbar

---
+++ Assets/Scripts/EnemyScripts/FloatingHealthbar.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingHealthbar : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    [SerializeField] private Camera camera;
+    [SerializeField] private Transform target;
+
+    public void UpdateHealthBar(float currentValue, float maxValue)
+    {
+        slider.value = currentValue / maxValue;
+    }
+
+    private void Update()
+    {
+        transform.rotation = camera.transform.rotation;
+        transform.position = target.position;
+    }
+}

a00b9ff0dff5a09c442f8d464976809ee35e14ed
vaangis93 <147488257+vaangis93@users.noreply.github.com>
2025-03-15 19:55:59 +0100

    Made a script for fireBullet. and firebullet destroys all objects that have tag "Destroyable"

---
+++ Assets/Scripts/WeaponScripts/Manage/FireBullet.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+
+public class FireBullet : MonoBehaviour, IProjectile
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] private int damage = 20;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision != null)
+        {
+            if (collision.gameObject.CompareTag("Enemy"))
+            {
+                Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+
+                if (enemy != null)
+                {
+                    enemy.TakeDamage(damage);
+                }
+                
+            }
+
+            if (collision.gameObject.CompareTag("Destroyable"))
+            {
+                Destroy(collision.gameObject);
+                Destroy(gameObject);
+            }
+            
+        }
+    }
+    
+  
+
+    public int GetDamage()
+    {
+        return damage;
+    }
+
+    public void SetDamage(int newDamage)
+    {
+        damage = newDamage;
+    }
+}

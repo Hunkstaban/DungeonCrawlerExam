2fb0b066bd59aa65bd97e878d5701f50381501b7
Tobias Ipsen <71149806+TobiasIpsen@users.noreply.github.com>
2025-03-19 10:20:16 +0100

    wip: shotgun

--- Assets/Scripts/WeaponScripts/Manage/Types/Gun.cs
+++ Assets/Scripts/WeaponScripts/Manage/Types/Gun.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,6 +11,9 @@ public class Gun : MonoBehaviour, IWeapon
     [SerializeField] private float speed = 20f;
 
     [SerializeField] private bool multiShotEnable = false;
+    [SerializeField] private bool shotgun = false;
+
+    private bool shotgunAllowedToShot = true;
     
     
     public void Attack()
@@ -17,6 +21,10 @@ public class Gun : MonoBehaviour, IWeapon
         if (multiShotEnable)
         {
             multiShotFire();
+        }
+        else if (shotgun)
+        {
+            StartCoroutine(ShotgunFire());
         }
         else
         {
@@ -37,6 +45,39 @@ public class Gun : MonoBehaviour, IWeapon
         Destroy(bulletInstance, 3);
     }
 
+    private IEnumerator ShotgunFire()
+    {
+        if (shotgunAllowedToShot)
+        {
+            shotgunAllowedToShot = false;
+            
+            Animator animator = GetComponent<Animator>();
+            animator?.SetTrigger("Shoot");
+            
+            Vector3[] bulletOffsets = new Vector3[]
+            {
+                muzzle.position + transform.forward + transform.right * 1.0f, // Right
+                muzzle.position + transform.forward - transform.right * 1.0f, // Left
+                muzzle.position + transform.forward + transform.up * 0.5f, // Up
+                muzzle.position + transform.forward - transform.up * 0.5f, // Down
+                muzzle.position + transform.forward * 2.0f // Center
+            };
+
+            foreach (Vector3 offset in bulletOffsets)
+            {
+                GameObject bulletInstance = Instantiate(projectile, offset, muzzle.rotation);
+                bulletInstance.GetComponent<Rigidbody>().AddForce(transform.forward * speed, ForceMode.Impulse);
+                Destroy(bulletInstance, 5);
+            }
+
+            ShotgunCooldown shotgunCooldown = GetComponent<ShotgunCooldown>();
+            shotgunCooldown?.StartCoroutine(shotgunCooldown.CoolDownDisplay(3));
+            
+            yield return new WaitForSeconds(3f);
+            shotgunAllowedToShot = true;
+        }
+    }
+    
     public void multiShotFire()
     {
         Vector3[] bulletOffsets = new Vector3[]
@@ -53,8 +94,8 @@ public class Gun : MonoBehaviour, IWeapon
             bulletInstance.GetComponent<Rigidbody>().AddForce(transform.forward * speed, ForceMode.Impulse);
             Destroy(bulletInstance, 5);
         }
-        
     }
+    
     public void AimAtTarget(Vector3 target)
     {
         transform.LookAt(target);
